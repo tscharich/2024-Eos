c57f210a7d9b7770bb1f68154645b66e8eda9bcf
Travis Scharich <tscharich2@gmail.com>
2024-02-25 22:44:10 -0500

    Updated WPI-Lib and Vendor Libraries.

--- src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
+++ src/main/java/frc/robot/subsystems/drive/GyroIOPigeon2.java
@@ -17,7 +17,6 @@ import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.StatusCode;
 import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.configs.Pigeon2Configuration;
-import com.ctre.phoenix6.configs.Pigeon2Configurator;
 import com.ctre.phoenix6.hardware.Pigeon2;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.util.Units;
